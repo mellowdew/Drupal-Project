4a047812b85540498348a220ab651fd700687bc6
mellodew <13.student.d8c.ru@mv105.initlab>
2022-08-29 16:30:23 +0300

    Three

--- 13.student.d8c.ru/web/modules/custom/hello_block/src/Controller/HelloController.php
+++ 13.student.d8c.ru/web/modules/custom/hello_block/src/Controller/HelloController.php
@@ -6,6 +6,7 @@ namespace Drupal\hello_world\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class HelloController extends ControllerBase {
+  
 
   public function hello() {
     return ['#markup' => $this->t("Hello World Module")];
